8bc530bcec917095b6dd9df658f6f09808ba4277
Lucas Assis <lukas.ecomp@gmail.com>
2025-07-27 23:07:47 -0300

    feat(docker): add docker compose file with postgres configuration

--- src/config/database-configuration.ts
+++ src/config/database-configuration.ts
@@ -1,8 +1,11 @@
 export default () => ({
   database: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
-      ? parseInt(process.env.DATABASE_PORT, 10)
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+      ? parseInt(process.env.DB_PORT, 10)
       : 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    databaseName: process.env.DB_DATABASE_NAME || 'financial_hub',
   },
 });
