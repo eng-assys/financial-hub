af88de996b0e7562f249adb7ce181cf6d9eafeeb
Lucas Assis <lukas.ecomp@gmail.com>
2025-07-28 21:50:29 -0300

    feat(swagger): add swagger definition to mais file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder()
+    .setTitle('Financial Hub example')
+    .setDescription('The Financial HUB API description')
+    .setVersion('1.0')
+    .addTag('financial-hub')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(configuration().app.port);
 }
 bootstrap();
